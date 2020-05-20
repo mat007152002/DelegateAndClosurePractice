987977f08ecb4b0ecd25a149dd4ef10cb0b5ec4e
Matthew Ling <mat007152002g@gmail.com>
2020-05-20 10:22:11 +0800

    Initial Commit

---
+++ DelegateAndClosurePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegateAndClosurePractice
+//
+//  Created by 旌榮 凌 on 2020/5/20.
+//  Copyright © 2020 旌榮 凌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
